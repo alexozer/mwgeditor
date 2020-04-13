33f7b88bfff387d10a2f223a57c76dcabb4adf6e
Alex Ozer <alexozer@users.noreply.github.com>
2020-04-12 23:44:58 -0400

    Show gravity ranges, optionally

--- editor/src/editor.cpp
+++ editor/src/editor.cpp
@@ -78,6 +78,9 @@ static std::shared_ptr<LevelModel> level = {};
 static VisualizationModel viz = {};
 static std::shared_ptr<ObjectModel> selectedObj = nullptr;
 
+static Texture gravRangeTex;
+static bool showGravRanges;
+
 static void showHelp()
 {
     ImGui::Begin("Help");
@@ -108,6 +111,28 @@ static void showLevelObject(ImDrawList *drawList, const std::shared_ptr<ObjectMo
     ImVec2 uv1(object->uvEnd());
 
     drawList->AddImage(object->tex.id, screenStart, screenEnd, uv0, uv1);
+
+    // Show gravity range if it's a planet
+    auto planet = std::dynamic_pointer_cast<PlanetModel>(object);
+    if (planet && showGravRanges)
+    {
+        // All gravity ranges seem to have this hard-coded scale at the moment...
+        constexpr float GRAV_RANGE_SCALE = 3.f;
+
+        float scaledGravWidth = gravRangeTex.width * object->scale / 5 * GRAV_RANGE_SCALE;
+        float scaledGravHeight = gravRangeTex.height * object->scale * GRAV_RANGE_SCALE;
+
+        ImVec2 worldRangeStart(object->pos.x - scaledGravWidth / 2, object->pos.y - scaledGravHeight / 2);
+        ImVec2 worldRangeEnd(object->pos.x + scaledGravWidth / 2, object->pos.y + scaledGravHeight / 2);
+
+        ImVec2 screenRangeStart = viz.worldToScreenSpace(worldRangeStart);
+        ImVec2 screenRangeEnd = viz.worldToScreenSpace(worldRangeEnd);
+
+        ImVec2 uv0(0.2, 0);
+        ImVec2 uv1(0.4, 1);
+
+        drawList->AddImage(gravRangeTex.id, screenRangeStart, screenRangeEnd, uv0, uv1);
+    }
 }
 
 static void showLevelGrid(ImDrawList *drawList) {
@@ -291,6 +316,8 @@ static void showPropertiesEditor()
     ImGui::SameLine();
     ImGui::Button("Add food");
 
+    ImGui::Checkbox("Show gravity ranges", &showGravRanges);
+
     ImGui::Separator();
 
     if (!selectedObj)
@@ -351,52 +378,52 @@ static void showPropertiesEditor()
     ImGui::End();
 }
 
-static void showDemoStuff()
-{
-    // Our state
-    static bool show_demo_window = true;
-    static bool show_another_window = false;
-
-    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-    if (show_demo_window)
-        ImGui::ShowDemoWindow(&show_demo_window);
-
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-    {
-        static float f = 0.0f;
-        static int counter = 0;
-
-        ImGui::Begin(
-                "Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-        ImGui::Checkbox("Another Window", &show_another_window);
-
-        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-//        ImGui::ColorEdit3("clear color", (float *) &clear_color); // Edit 3 floats representing a color
-
-        if (ImGui::Button(
-                "Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-            counter++;
-        ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
-
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
-                    ImGui::GetIO().Framerate);
-        ImGui::End();
-    }
-
-    // 3. Show another simple window.
-    if (show_another_window) {
-        ImGui::Begin("Another Window",
-                     &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-        ImGui::Text("Hello from another window!");
-        if (ImGui::Button("Close Me"))
-            show_another_window = false;
-        ImGui::End();
-    }
-}
+//static void showDemoStuff()
+//{
+//    // Our state
+//    static bool show_demo_window = true;
+//    static bool show_another_window = false;
+//
+//    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+//    if (show_demo_window)
+//        ImGui::ShowDemoWindow(&show_demo_window);
+//
+//    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+//    {
+//        static float f = 0.0f;
+//        static int counter = 0;
+//
+//        ImGui::Begin(
+//                "Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+//
+//        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+//        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+//        ImGui::Checkbox("Another Window", &show_another_window);
+//
+//        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+////        ImGui::ColorEdit3("clear color", (float *) &clear_color); // Edit 3 floats representing a color
+//
+//        if (ImGui::Button(
+//                "Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+//            counter++;
+//        ImGui::SameLine();
+//        ImGui::Text("counter = %d", counter);
+//
+//        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
+//                    ImGui::GetIO().Framerate);
+//        ImGui::End();
+//    }
+//
+//    // 3. Show another simple window.
+//    if (show_another_window) {
+//        ImGui::Begin("Another Window",
+//                     &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+//        ImGui::Text("Hello from another window!");
+//        if (ImGui::Button("Close Me"))
+//            show_another_window = false;
+//        ImGui::End();
+//    }
+//}
 
 void initEditor()
 {
@@ -412,11 +439,14 @@ void initEditor()
             selectedObj = planet;
         }
     }
+
+    IM_ASSERT(loadTextureFromFile("../assets/textures/range.png", gravRangeTex));
+    showGravRanges = true;
 }
 
 void runEditor()
 {
-    showDemoStuff();
+//    showDemoStuff();
     showHelp();
     showLevelVisualization();
     showPropertiesEditor();
